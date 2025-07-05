9a3c56280afea0919698f8c60330107389dce399
Nate Orange <cc.lemonhead@gmail.com>
2025-07-05 13:33:39 -0400

    chore(types): add temporary re-export alias to unified TCC (phase-1 cleanup)

---
+++ src/lib/types/product-tool-creation-v2/tcc-alias.ts
@@ -0,0 +1,5 @@
+// Temporary re-export shim while we migrate all code to the unified TCC types.
+// Once every import is switched to "@/lib/types/tcc-unified", this file (and
+// its usages) can be deleted.
+
+export * from '../tcc-unified';
